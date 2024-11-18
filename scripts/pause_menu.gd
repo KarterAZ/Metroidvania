c7857a477376ed3ce19cdda4044ce36f491b4a0a
321pie <aprspie@gmail.com>
2024-11-18 08:27:14 -0800

    Added pause menu

---
+++ scripts/pause_menu.gd
@@ -0,0 +1,27 @@
+extends Control
+
+@onready var menu = $"."
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	open_close_menu()
+	
+func pause() -> void:
+	get_tree().paused = true
+	menu.visible = true
+		
+func resume() -> void:
+	get_tree().paused = false
+	menu.visible = false
+
+func open_close_menu() -> void:
+	if Input.is_action_just_pressed("Menu") and !get_tree().paused:
+		pause()
+	elif Input.is_action_just_pressed("Menu") and get_tree().paused:
+		resume()
+
+func _on_resume_game_pressed() -> void:
+	resume()
+
+func _on_exit_game_pressed() -> void:
+	get_tree().quit()
