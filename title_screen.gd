7e8b832d600dc39b14cc3a142566a12bff8c9e92
321pie <aprspie@gmail.com>
2024-11-17 20:56:51 -0800

    Got a title screen and started messing with things

---
+++ title_screen.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@onready var start = $MarginContainer/Menu_Options/Start_Game
+@onready var end = $MarginContainer/Menu_Options/Quit_Game
+var starting_room = "res://Starting_Room.tscn"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	start.connect("button_up", start_game)
+	end.connect("button_up", end_game)
+
+func start_game() -> void:
+	get_tree().change_scene_to_file(starting_room)
+
+func end_game() -> void:
+	get_tree().quit()
