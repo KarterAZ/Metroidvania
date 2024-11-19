29963bef80c8b613cc7b50545f101de26a64494a
321pie <aprspie@gmail.com>
2024-11-18 18:51:50 -0800

    Added player

---
+++ scripts/player.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+
+@export var jump_force = 1500
+@export var speed = 50
+@export var gravity = 50
+
+@export var min_speed = 50
+@export var max_speed = 1000
+
+@export var min_gravity = 50
+@export var max_gravity = 1000
+
+@onready var sprite = $Player_Sprite
+
+func _physics_process(delta):
+	if !is_on_floor():
+		velocity.y = velocity.y + gravity if gravity < max_gravity else max_gravity
+		gravity += 5
+		
+	else:
+		gravity = min_gravity
+		if Input.is_action_just_pressed("Jump"):
+			velocity.y = -jump_force
+	
+	var horizontal_direction = Input.get_axis("Left", "Right")
+	if horizontal_direction != 0:
+		speed += 25
+		sprite.flip_h = (horizontal_direction == 1)
+	else:
+		speed = min_speed
+	velocity.x = horizontal_direction * speed if speed < max_speed else horizontal_direction * max_speed
+	
+	move_and_slide()
